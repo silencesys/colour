4a545712515eead26e3aff16623e18ff32473752
Martin Rocek <martin@oculum.ink>
2016-04-29 12:53:45 +0200

    new repo link

--- layout/header.php
+++ layout/header.php
@@ -44,7 +44,7 @@
             <nav>
                 <ul>
                     <li><a href="<?php echo $base_url ?>api">Api</a></li>
-                    <li><a href="https://gitlab.com/Silencesys/colorizer">Gitlab</a></li>
+                    <li><a href="https://gitlab.com/Silencesys/colour">Gitlab</a></li>
                     <li><a href="<?php echo $base_url ?>authors">Authors</a></li>
                     <li><a href="<?php echo $base_url ?>about">About</a></li>
                 </ul>
