009b0f8ecc1b76b93bb890d420610b6389e75fb7
Martin Rocek <martin@oculum.ink>
2016-05-01 13:12:26 +0200

    gitignore, typo fixes

--- layout/about.php
+++ layout/about.php
@@ -3,11 +3,11 @@
     <h2>About</h2>
 
     <p>
-        Colours can do much more than we can even imagine. With colors, you can express your feelings, set a mood or just use them as a starting point for your creations. The goal of Colours is to make our everyday life colorful. Thanks to a simple color generator, there's a new color waiting for you each time you visit the website. But you can also generate new random colors using the button next to the color code at the bottom of the page, or enter your own color code and see how the color looks like. Colours supports two color code formats - RGB (e.g.: 255,0,255) and HEX (e.g. ff00ff or ccc).
+        Colour can do much more than we can even imagine. With colors, you can express your feelings, set a mood or just use them as a starting point for your creations. The goal of Colour is to make our everyday life colorful. Thanks to a simple color generator, there's a new color waiting for you each time you visit the website. But you can also generate new random colors using the button next to the color code at the bottom of the page, or enter your own color code and see how the color looks like. "Colour" supports two color code formats - RGB (e.g.: 255,0,255) and HEX (e.g. ff00ff or ccc).
     </p>
 
     <p>
-        Did you find your favourite color or color that expresses your feelings? You sure did. Let your friends know about that color thanks to social networks. Colours offers sharing via Google+, Facebook and Twitter. Do you want to save your color for later? save the page with the color to you bookmarks in your web browser.
+        Did you find your favourite color or color that expresses your feelings? You sure did. Let your friends know about that color thanks to social networks. Do you want to save your color for later? save the page with the color to you bookmarks in your web browser.
     </p>
     <p>
         Maybe is good to know that current colour is #<?php echo $color['hex']; ?>.
--- modules/tips.php
+++ modules/tips.php
@@ -10,13 +10,13 @@ function RandomTip() {
 	6 => "<b>Tip:</b> Did you know that Martin's most favourite colour is #ef21de?",
 	7 => "<b>Tip:</b> Did you know that Yas hates colour #ef21de?",
 	8 => "We're terribly sorry, if you're colourblind.<br> Maybe, we'll release a service for you sometime later.",
-	9 => "<b>Tip:</b> Got a better idea for a tip than those we already have?<br> Let us know at color[at]oculum.ink.",
+	9 => "<b>Tip:</b> Got a better idea for a tip than those we already have?<br> Let us know at colour[at]oculum.ink.",
 	10 => "<b>Tip:</b> Error 37.",
 	11 => "<b>Tip:</b> All colors for Colorizer adverts were generated using Colorizer.",
 	12 => "<b>Tip:</b> Share your favorite colour on social networks.",
 	13 => "<b>Tip:</b> No idea what colour to pick? Colorizer will help you.",
 	14 => "<b>Tip:</b> Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn",
-	15 => "<b>Tip:</b> [Your ad here]",
+	15 => "[Your ad here]",
 	16 => "<b>Tip:</b> Did you know that red pandas do never fart?"
 	);
 	return ($tips[$number]); // Random generated number
